9ebc46fee11db42ae139c7786799b413ff8acbcb
mehulrnsit <mehulsvps000@gmail.com>
2022-10-02 17:40:29 +0530

    deployment-changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />}/>
+          <Route path="/wonderSearch" element={<LandingPage />}/>
           <Route path="/home" element={<Home/>} />
         </Routes>
       </BrowserRouter>
